2bdd6f79da338abe4c4f173e32282642941b97ad
Marc <strategy155@gmail.com>
2016-10-18 04:25:51 +0300

    refactored

--- test_soup.py
+++ test_soup.py
@@ -8,9 +8,21 @@ from nltk.tree import ParentedTree
 import csv
 import sys
 
+
+def _proccess_string(inp_str):
+    inp_str = inp_str.replace('\n', ' ')
+    inp_str = inp_str.replace('!', '.')
+    inp_str = inp_str.replace('?', '.')
+    inp_str = inp_str.replace('\t', '')
+    inp_str = inp_str.replace('[', '')
+    inp_str = inp_str.replace(']', '')
+    inp_str = inp_str.rstrip('\t')
+    inp_str = inp_str.replace(';', '.')
+    inp_str = inp_str.replace(':', '.')
+    return inp_str
+
 maxInt = sys.maxsize
 decrement = True
-
 while decrement:
     # decrease the maxInt value by factor 10
     # as long as the OverflowError occurs.
@@ -22,70 +34,61 @@ while decrement:
         maxInt = int(maxInt/10)
         decrement = True
 
-for d, dirs, files in os.walk(os.path.normpath('C:\\Users\\Marc\\PycharmProjects\\nltk\\texts')):
-    for fe in files:
-        path=os.path.join(d,fe)
+for root, dirs, files in os.walk(os.path.normpath('C:\\Users\\Marc\\PycharmProjects\\nltk\\texts')):
+    for current_file in files:
+        path=os.path.join(root, current_file)
         prs_path=os.path.normpath('C:\\Users\\Marc\\PycharmProjects\\nltk\\prses')
-        f=open(path,encoding='ISO-8859-1')
+        current_file_opened=open(path, encoding='ISO-8859-1')
         prs_file=os.path.join(prs_path, os.path.join(path.split('\\')[-2], path.split('\\')[-1].split('.')[0]+'.prs'))
         try:
             open(prs_file)
         except FileNotFoundError:
             continue
-        flag1=0
+        is_parsed=0
         with open(prs_file, encoding='utf-8') as tsv_inp:
-            lulcsv = list(csv.reader(tsv_inp, dialect='excel-tab'))
-            for line in lulcsv:
+            prs_csv_list = list(csv.reader(tsv_inp, dialect='excel-tab'))
+            for line in prs_csv_list:
                 if not line[0].startswith('#'):
                     if '_mmbr' in line[13]:
-                        flag1=1
+                        is_parsed=1
                         break
-            if flag1==1:
+            if is_parsed==1:
                 continue
-        soup=utils.make_soup_with_webpage(f,'html')
-        lul=utils.get_text(soup)
+        soup=utils.make_soup_with_webpage(current_file_opened, 'html')
+        text_src=utils.get_text(soup)
         parser = StanfordParser()
-        lul = lul.replace('\n', ' ')
-        lul = lul.replace('!','.')
-        lul = lul.replace('?','.')
-        lul = lul.replace('\t','')
-        lul = lul.replace('[','')
-        lul = lul.replace(']', '')
-        lul=lul.rstrip('\t')
-        lul = lul.replace(';','.')
-        lul=lul.replace(':','.')
-        lu = lul
-        lul=[]
-        for l in lu.split('.'):
+
+        text_src_unchanged = _proccess_string(text_src)
+        text_src=[]
+        for l in text_src_unchanged.split('.'):
             if l != "" and len(l.split())<100:
-                lul.append(l+'.')
+                text_src.append(l + '.')
             else:
                 l=l.replace(',','.')
                 for e in l.split('.'):
                     if e != "":
-                        lul.append(e+'.')
-        lul.pop()
-        print(lul)
-        sents = parser.raw_parse_sents(lul)
+                        text_src.append(e + '.')
+        text_src.pop()
+        print(text_src)
+        parsed_text = parser.raw_parse_sents(text_src)
 
         with open(prs_file, 'w+', encoding='utf-8', newline='') as tsv_out:
-            ulucsv = csv.writer(tsv_out, dialect='excel-tab')
+            output_prs = csv.writer(tsv_out, dialect='excel-tab')
             word_list=[]
-            for lines in sents:
+            for lines in parsed_text:
                 for sent in lines:
                         ptree = ParentedTree.convert(sent)
                         for sub in ptree.subtrees():
-                            flag=True
                             if str(sub).count('(') == 1:
                                 word_list.append(sub)
             print(word_list)
 
-            for line in lulcsv:
+            for line in prs_csv_list:
                 for sub in word_list:
                     lulue=str(sub.leaves()[0])
                     if not line[0].startswith('#'):
                         if str(sub.leaves()[0]) == line[4] and (line[8]=='1' or line[8]=='?'):
                             line[13] += (' '+str(sub.parent()).split('(')[1].replace('\n','').replace(' ','')+'_mmbr')
                             break
-                ulucsv.writerow(line)
+                output_prs.writerow(line)
 
